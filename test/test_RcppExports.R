866807450927b23efea2b4c503753bfaf5eafe15
unknown <black-simon@hotmail.de>
2020-03-21 19:37:54 +0100

    add cn tests and markdown

--- test/test_RcppExports.R
+++ test/test_RcppExports.R
@@ -3,7 +3,6 @@
 library(testthat)
 library(Rcpp)
 library(GCATR)
-print(code_check_if_k_circular(1,'UGGGUG',3))
 
 # Maximal circularity:
 
@@ -29,7 +28,7 @@ test_that('is non circular, string vector', {
 })
 
 
-test_that('is ircular, string vector', {
+test_that('is circular, string vector', {
   expect_true(code_check_if_circular(c('GGU','GGC','ACU','ACC','AGC','AGU','GAC','GAU','GUC','GUU','AAU','AUU','AAC','AUC','GCU','GCC')))
 })
 
@@ -81,8 +80,38 @@ test_that('is non circular, string vector', {
 test_that('is circular,  string vector', {
   expect_true(code_check_if_k_circular(1,c('UGG','CUG','GGC','UGU')))
   expect_true(code_check_if_k_circular(2,c('ACG','GUA','CGU','CGG','UAC')))
-  expect_true(code_check_if_k_circular(3,c('CGU', 'ACG', 'UAC', 'GUA',)))
+  expect_true(code_check_if_k_circular(3,c('CGU', 'ACG', 'UAC', 'GUA')))
   expect_true(code_check_if_k_circular(4,c('GGU', 'GGC', 'ACU', 'ACC', 'AGC', 'AGU', 'GAC', 'GAU', 'GUC', 'GUU', 'AAU', 'AUU', 'AAC', 'AUC', 'GCU', 'GCC')))
 })
+
+# cn-circularity
+
+test_that('is non-circular, string with no delimiter, tuple length 3', {
+  expect_false(code_check_if_cn_circular('UGGGUG', 3))
+})
+
+test_that('is cn-circular, string with no delimiter, tuple length 3', {
+  expect_true(code_check_if_cn_circular('GGUGGCACUACCAGCAGUGACGAUGUCGUUAAUAUUAACAUCGCUGCC ', 3))
+})
+
+
+test_that('is non cn-circular, string split by " "', {
+  expect_false(code_check_if_cn_circular('UGG GUG'))
+})
+
+test_that('is cn-circular, string split by " "', {
+  expect_true(code_check_if_cn_circular('GGU GGC ACU ACC AGC AGU GAC GAU GUC GUU AAU AUU AAC AUC GCU GCC'))
+})
+
+test_that('is non cn-circular, string vector', {
+  expect_false(code_check_if_cn_circular(c('UGG', 'GUG')))
+})
+
+
+test_that('is cn-circular, string vector', {
+  expect_true(code_check_if_cn_circular(c('GGU','GGC','ACU','ACC','AGC','AGU','GAC','GAU','GUC','GUU','AAU','AUU','AAC','AUC','GCU','GCC')))
+})
+
+
 
 
