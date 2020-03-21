fbf170202d24e2845997018904bf5d545cd246f1
unknown <black-simon@hotmail.de>
2020-03-21 19:06:03 +0100

    added notebook after sorting out loads of problems due to version incompatibility

--- test/test_RcppExports.R
+++ test/test_RcppExports.R
@@ -1,141 +1,88 @@
 # Check circularity functions
 
-# Maximal circularity:
-
-# 1. non circular, string with no delimiter, tuple length 3
 library(testthat)
 library(Rcpp)
 library(GCATR)
-print(code_check_if_k_circular(2,'UGGCUGGGCUGU',3))
+print(code_check_if_k_circular(1,'UGGGUG',3))
 
-test_that('is not circular', {
-    expect_false(code_check_if_circular('UGGGUG', 3))
+# Maximal circularity:
+
+test_that('is non-circular, string with no delimiter, tuple length 3', {
+  expect_false(code_check_if_circular('UGGGUG', 3))
 })
 
-# 2.circular, string with no delimiter, tuple length 3
-test_that('is circular', {
+test_that('is circular, string with no delimiter, tuple length 3', {
   expect_true(code_check_if_circular('GGUGGCACUACCAGCAGUGACGAUGUCGUUAAUAUUAACAUCGCUGCC ', 3))
 })
 
-# 3. non circular, string split by ' '
-test_that('is not circular', {
+
+test_that('is non circular, string split by " "', {
   expect_false(code_check_if_circular('UGG GUG'))
 })
 
-# 4.circular, string split by ' '
-test_that('is circular', {
+test_that('is circular, string split by " "', {
   expect_true(code_check_if_circular('GGU GGC ACU ACC AGC AGU GAC GAU GUC GUU AAU AUU AAC AUC GCU GCC'))
 })
 
-# 5.non circular, string vector
-test_that('is circular', {
+test_that('is non circular, string vector', {
   expect_false(code_check_if_circular(c('UGG', 'GUG')))
 })
 
 
-# 6.circular, string vector
-test_that('is circular', {
+test_that('is ircular, string vector', {
   expect_true(code_check_if_circular(c('GGU','GGC','ACU','ACC','AGC','AGU','GAC','GAU','GUC','GUU','AAU','AUU','AAC','AUC','GCU','GCC')))
 })
 
 # k- circularity:
 
-# 1. non 1-circular, string with no delimiter, tuple length 3
-test_that('is not 1-circular', {
+test_that('is non circular, string with no delimiter, tuple length 3', {
   expect_false(code_check_if_k_circular(1,'UGGGUG', 3))
+  expect_false(code_check_if_k_circular(2,'UGGCUGGGCUGU',3))
+  expect_false(code_check_if_k_circular(3,'ACGGUACGUCGGUAC',3))
+  expect_false(code_check_if_k_circular(4,'CGUACGUACGUA',3))
+
+  
 })
 
-# 2. 1-circular, string with no delimiter, tuple length 3
-test_that('is not 1-circular', {
+test_that('is circular, string with no delimiter, tuple length 3', {
   expect_true(code_check_if_k_circular(1,'UGGCUGGGCUGU', 3))
+  expect_true(code_check_if_k_circular(2,'ACGGUACGUCGGUAC',3))
+  expect_true(code_check_if_k_circular(3,'CGUACGUACGUA',3))
+  expect_true(code_check_if_k_circular(4,'GGUGGCACUACCAGCAGUGACGAUGUCGUUAAUAUUAACAUCGCUGCC',3))
+
+  
+  
 })
 
-# 3. non 1-circular, string split by ' '
-test_that('is not 1-circular', {
+test_that('is non circular, string split by " "', {
   expect_false(code_check_if_k_circular(1,'UGG GUG'))
+  expect_false(code_check_if_k_circular(2,'UGG CUG GGC UGU'))
+  expect_false(code_check_if_k_circular(3,'ACG GUA CGU CGG UAC'))
+  expect_false(code_check_if_k_circular(4,'CGU ACG UAC GUA'))
 })
 
-# 4. 1-circular,  string split by ' '
-test_that('is not 1-circular', {
+test_that('is circular, string split by " "', {
   expect_true(code_check_if_k_circular(1,'UGG CUG GGC UGU'))
+  expect_true(code_check_if_k_circular(2,'ACG GUA CGU CGG UAC'))
+  expect_true(code_check_if_k_circular(3,'CGU ACG UAC GUA'))
+  expect_true(code_check_if_k_circular(4,'GGU GGC ACU ACC AGC AGU GAC GAU GUC GUU AAU AUU AAC AUC GCU GCC'))
+  
 })
 
-# 5. non 1-circular, string vector
-test_that('is not 1-circular', {
+# 5. non circular, string vector
+test_that('is non circular, string vector', {
   expect_false(code_check_if_k_circular(1,c('UGG','GUG')))
+  expect_false(code_check_if_k_circular(2,c('UGG', 'CUG', 'GGC', 'UGU')))
+  expect_false(code_check_if_k_circular(3,c('ACG', 'GUA', 'CGU', 'CGG', 'UAC')))
+  expect_false(code_check_if_k_circular(4,c('CGU', 'ACG', 'UAC', 'GUA')))
+  
 })
-# 6. 1-circular,  string vector
-test_that('is not 1-circular', {
+# 6. circular,  string vector
+test_that('is circular,  string vector', {
   expect_true(code_check_if_k_circular(1,c('UGG','CUG','GGC','UGU')))
-})
-
-# 7. non 2-circular, string with no delimiter, tuple length 3
-test_that('is not 2-circular', {
-  expect_false(code_check_if_k_circular(2,'UGGCUGGGCUGU',3))
-})
-
-# 8.  2-circular, string with no delimiter, tuple length 3
-test_that('is not 2-circular', {
-  expect_true(code_check_if_k_circular(2,'ACGGUACGUCGGUAC',3))
-})
-
-# 9. non 2-circular, string split by ' '  
-test_that('is not 2-circular', {
-  expect_false(code_check_if_k_circular(2,'UGG CUG GGC UGU'))
-})
-
-# 10.  2-circular, string split by ' '
-test_that('is not 2-circular', {
-  expect_true(code_check_if_k_circular(2,'ACG GUA CGU CGG UAC',3))
-})
-
-# 11. non 2-circular, string vector  
-test_that('is not 2-circular', {
-  expect_false(code_check_if_k_circular(2,c('UGG','CUG','GGC','UGU')))
-})
-
-# 12.  2-circular, string vector'
-test_that('is not 2-circular', {
   expect_true(code_check_if_k_circular(2,c('ACG','GUA','CGU','CGG','UAC')))
+  expect_true(code_check_if_k_circular(3,c('CGU', 'ACG', 'UAC', 'GUA',)))
+  expect_true(code_check_if_k_circular(4,c('GGU', 'GGC', 'ACU', 'ACC', 'AGC', 'AGU', 'GAC', 'GAU', 'GUC', 'GUU', 'AAU', 'AUU', 'AAC', 'AUC', 'GCU', 'GCC')))
 })
-
-# 13. 1-circular,  string vector
-test_that('is not 1-circular', {
-  expect_true(code_check_if_k_circular(1,c('UGG','CUG','GGC','UGU')))
-})
-
-# 14. non 2-circular, string with no delimiter, tuple length 3
-test_that('is not 2-circular', {
-  expect_false(code_check_if_k_circular(2,'UGGCUGGGCUGU',3))
-})
-
-# 15.  2-circular, string with no delimiter, tuple length 3
-test_that('is not 2-circular', {
-  expect_true(code_check_if_k_circular(2,'ACGGUACGUCGGUAC',3))
-})
-
-# 16. non 2-circular, string split by ' '  
-test_that('is not 2-circular', {
-  expect_false(code_check_if_k_circular(2,'UGG CUG GGC UGU'))
-})
-
-# 17.  2-circular, string split by ' '
-test_that('is not 2-circular', {
-  expect_true(code_check_if_k_circular(2,'ACG GUA CGU CGG UAC',3))
-})
-
-# 18. non 2-circular, string vector  
-test_that('is not 2-circular', {
-  expect_false(code_check_if_k_circular(2,c('UGG','CUG','GGC','UGU')))
-})
-
-# 12.  2-circular, string vector'
-test_that('is not 2-circular', {
-  expect_true(code_check_if_k_circular(2,c('ACG','GUA','CGU','CGG','UAC')))
-})
-
-
-
-
 
 
