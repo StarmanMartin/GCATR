f128ca680ad8eb28a06ee7e6a8fa757ea8c3126c
unknown <black-simon@hotmail.de>
2020-04-20 18:01:33 +0200

    add missing semicolon

--- src/RAdapterUtils.cpp
+++ src/RAdapterUtils.cpp
@@ -7,7 +7,7 @@
 
 std::vector <std::string> RAdapterUtils::as_cpp_string_vector(StringVector r_vec) {
   if(r_vec.length() == 0) {
-      throw std::invalid_argument("Vector should not be empty!")
+      throw std::invalid_argument("Vector should not be empty!");
   }
   std::vector <std::string> vstrings(r_vec.size());
   int i;
